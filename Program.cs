edb2f5156f353571e5f71452ee66b643693c00cf
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-09 15:16:08 +1000

    :tada: Assignmnet 2 project Initalized

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADS103_Grishma_A00177960_Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
